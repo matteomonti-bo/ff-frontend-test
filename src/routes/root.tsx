3bb55097944c96aa1dc67152557d9f27477467a6
Matteo Monti <m.monti@futurefashionsolution.com>
2023-05-07 12:22:25 +0200

    header component

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const Root = () => {
   return (
     <>
-      <div>hello world!</div>
-       <Outlet />
+      <Header />
+      <Outlet />
     </>
   )
 }
