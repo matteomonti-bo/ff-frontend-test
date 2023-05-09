427bcde556bde525de0eb2ddc167aec6caa12704
Matteo Monti <m.monti@futurefashionsolution.com>
2023-05-09 23:20:25 +0200

    context provider

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,15 +1,17 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { CartContext } from "../context/cartContext";
 import Header from "../components/Header";
 
 const Root = () => {
+  const [itemsCount, setItemsCount] = useState(0)
   return (
-    <>
+    <CartContext.Provider value={{itemsCount, setItemsCount}}>
       <Header />
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
-    </>
+    </CartContext.Provider>
   )
 }
 
