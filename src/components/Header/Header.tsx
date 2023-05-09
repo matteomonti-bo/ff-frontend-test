27c72c988881beee8b3d7c896cb5bab9ae2803ac
Matteo Monti <m.monti@futurefashionsolution.com>
2023-05-09 20:42:07 +0200

    fix: add logo link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from '../../images/logo.svg';
-import { ReactComponent as LogoLarge } from '../../images/logo-large.svg';
-import { ReactComponent as Bag } from '../../images/bag.svg';
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { ReactComponent as LogoLarge } from "../../images/logo-large.svg";
+import { ReactComponent as Bag } from "../../images/bag.svg";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoWrapper}>
-        <Logo className={styles.logo} />
-        <LogoLarge className={styles.logoLarge} />
-        <span className='visually-hidden'>Future Fashion Logo</span>
+        <Link to={`/`} className={styles.logoLink}>
+          <Logo className={styles.logo} />
+          <LogoLarge className={styles.logoLarge} />
+          <span className="visually-hidden">Future Fashion Logo</span>
+        </Link>
       </div>
       <div className={styles.infoWrapper}>
-        <Link to={`/`} className={styles.link}>SHOP</Link>
+        <Link to={`/`} className={styles.link}>
+          SHOP
+        </Link>
         <Bag className={styles.bag} />
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
