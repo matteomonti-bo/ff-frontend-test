a748a8b8c2351bdfe6f4fe3ef2e5425c1495a0f5
Matteo Monti <info@montiwebd.net>
2023-05-30 16:20:02 +0200

    mocks

---
+++ src/mocks/products.ts
@@ -0,0 +1,34 @@
+import { ProductPreview } from "../models";
+
+export const singleLiteProduct: ProductPreview = { "id": 1, "title": "iPhone 9", "category": "smartphones", "brand": "Apple", "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg"};
+
+export const productLiteList: ProductPreview[] = [
+  {
+    "id": 1,
+    "title": "iPhone 9",
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+  },
+  {
+    "id": 2,
+    "title": "iPhone X",
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Samsung Universe 9",
+    "brand": "Samsung",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg"
+  },
+  {
+    "id": 4,
+    "title": "OPPOF19",
+    "brand": "OPPO",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+  }
+]
